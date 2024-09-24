bee9bb96dea20d084355226a7b0cc3c6e5d1d1a3
livingamel <livingamel@myvuw.ac.nz>
2024-09-24 23:12:39 +1200

    Create api and controller packages. Set up gson and PokedexAPI

---
+++ src/main/api/PokedexAPI.java
@@ -0,0 +1,41 @@
+package main.api;
+
+import main.pokemon.Pokemon;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PokedexAPI {
+    private static final String URL = "https://pokeapi.co/api/v2/pokemon/";
+    //Number of Pokemon to call at a time.
+    private final int limit = 20; // able to change this?
+
+    public List<Pokemon> getPokemonList(int offset) throws Exception {
+        URL url = new URL(URL + "?offset=" + offset + "&limit=" + limit);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        if (connection.getResponseCode() != 200) {
+            throw new RuntimeException("Failed: HTTP error code: " + connection.getResponseCode());
+        }
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return parsePokemonList(response.toString());
+    }
+
+    private List<Pokemon> parsePokemonList(String json) {
+        List<Pokemon> pokemonList = new ArrayList<>();
+        return pokemonList;
+    }
+
+}
